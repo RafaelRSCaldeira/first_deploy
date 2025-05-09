c0570d2c474c407a3755daf419c1e6d4c3295aa4
Rafael <rafael.caldeira.2005@gmail.com>
2025-05-09 20:56:00 -0300

    new methods added

--- app.py
+++ app.py
@@ -1,9 +1,31 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+values =[
+    {
+        "id": 1,
+        "name": "Roberto"
+    },
+    {
+        "id": 2,
+        "name": "Rafael"
+    }
+]
 @app.post("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "Hello, World!"
+
+@app.route("/")
+def hello():
+    return "<h1>Olá Mundo</h1>"
+
+@app.get("/<int:id>/")
+def get_one(id):
+    for item in values:
+        if item["id"] == id:
+            return jsonify(item)
+    return jsonify({"error": "Not found"}), 404
+
 if __name__ == "__main__":
     app.run(debug=True)
